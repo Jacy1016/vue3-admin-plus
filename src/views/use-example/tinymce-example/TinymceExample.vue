f8d595dac690ccbf2201e10d3379ba651f094ed6
jacy <jacy35@163.com>
2022-02-07 14:40:42 +0800

    init

---
+++ src/views/use-example/tinymce-example/TinymceExample.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="scroll-y">
+    <div class="mb-1">
+      <el-button @click="setTinyContent">设置文本消息</el-button>
+      <el-button @click="getTinyContent">获取文本消息</el-button>
+    </div>
+    <Tinymce ref="refTinymce" />
+    <div class="mt-2">只读,readOnly为true</div>
+    <Tinymce :read-only="true" />
+  </div>
+</template>
+
+<script setup>
+import { getCurrentInstance, ref } from 'vue'
+import Tinymce from '@/components/Tinymce/Tinymce.vue'
+//获取store和router
+// import {useRouter} from 'vue-router'
+// import {useStore} from 'vuex'
+let { proxy } = getCurrentInstance()
+/*tinymce操作*/
+const refTinymce = ref(null)
+const setTinyContent = () => {
+  if (refTinymce?.value) {
+    refTinymce.value.setContent('tinymce设置的内容')
+  }
+}
+const getTinyContent = () => {
+  if (refTinymce?.value) {
+    let content = refTinymce.value.getContent()
+    proxy.elMessageMixin(content)
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>
